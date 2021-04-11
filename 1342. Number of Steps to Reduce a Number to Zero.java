d16211edee322d13418584369d8f4c858411df02
lilly-weng <79388715+lilly-weng@users.noreply.github.com>
2021-04-11 17:41:12 -0500

    Create 1342. Number of Steps to Reduce a Number to Zero.java

---
+++ 1342. Number of Steps to Reduce a Number to Zero.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        if (num == 0) return 0;
+        
+        int steps = 0;
+        while (num != 0) {
+            if (num%2 == 0) {
+                num = num/2;
+                steps += 1;
+            } else {
+                num = num - 1;
+                steps += 1;
+            }
+        }
+        
+        return steps;
+    }
+}
